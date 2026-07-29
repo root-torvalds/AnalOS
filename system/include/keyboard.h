47a7697c034fbaeba2c3240c8f578e8aee5ee360
root-torvalds <larcenkokirill91@gmail.com@google.com>
2026-07-29 18:25:47 +0300

    Add jetbrains font

--- system/include/keyboard.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void keyboard_handler_c();
--- system/include/mouse.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-void mouse_wait_write();
-void mouse_wait_read();
-void mouse_write(uint8_t data);
-uint8_t mouse_read();
-void init_mouse();
-void mouse_handler_c();
